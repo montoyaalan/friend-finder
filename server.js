6ca951e0af6382b340f1621254e5e9b22c6bd118
Alan Montoya <alanmontoya2010@att.net>
2019-06-11 09:35:13 -0500

    public directory added along with express

--- server.js
+++ server.js
@@ -14,3 +14,11 @@ const PORT = process.env.PORT || 3000;
 
 const directory_public = path.join(__dirname, "app", "public");
 const directory_routes = path.join(__dirname, "app", "routes");
+
+// Set public directory
+app.use(express.static(directory_public));
+
+// Set up Express to handle parsing data
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({"extended": true}));
