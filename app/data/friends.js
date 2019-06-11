38e21f2835cb1ebca387c2f272aa221fb72ec533
Alan Montoya <alanmontoya2010@att.net>
2019-06-11 11:50:49 -0500

    conmection to sql

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,3 +7,14 @@
 *****************************************************************************
 *****************************************************************************/
 const mysql = require("mysql");
+
+// Connect to MySQL
+const pool = mysql.createPool({
+    "connectionLimit"   : 10,
+    "host"              : "ysp9sse09kl0tzxj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "port"              : 3306,
+    "user"              : "l7g2iikw6shnok4f",
+    "password"          : "atw1sx49swurflkj",
+    "database"          : "yfpdqkylr03ge45y",
+    "multipleStatements": true
+});
