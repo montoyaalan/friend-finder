6cbe21882f3ad704e503e7c5bc0e4cb861765e17
Alan Montoya <alanmontoya2010@att.net>
2019-06-11 18:35:55 -0500

    set up routes inside apiroutes file

--- app/routes/api_routes.js
+++ app/routes/api_routes.js
@@ -15,3 +15,15 @@ const router = express.Router();
 // Create an instance of FriendFinder
 const FriendFinder = require(path.join(__dirname, "..", "data", "friends.js"));
 const friendFinder = new FriendFinder();
+
+/****************************************************************************
+ ****************************************************************************
+    
+    Set up routes
+    
+*****************************************************************************
+*****************************************************************************/
+// Display all friends
+router.get("/friends", (req, res) => {
+    res.json(friendFinder.getFriends());
+});
